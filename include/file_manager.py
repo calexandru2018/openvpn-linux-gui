99a93dfa043671dd32340a0c921f5ce88e2aa432
Alexandru Cheltuitor <31934100+calexandru2018@users.noreply.github.com>
2019-10-13 22:12:04 +0200

    Added docstrings

--- include/file_manager.py
+++ include/file_manager.py
@@ -5,12 +5,45 @@ class FileManager():
 		self.dirPath = dirPath
 
 	def returnFileExist(self, folderName, fileName, fileType):
+		'''Checks if file exists in specified folder.
+		
+		Parameters:
+		----------
+		`folderName` : string
+			The name of the folder.
+		`fileName` : string
+			The name of the file.
+		`fileType` : string
+			The type/extension - json or txt.
+		Returns:
+		-------
+		bool:
+			Returns True if file exists, False otherwise.
+		'''
 		if(os.path.isfile(f"./{folderName}/{fileName}.{fileType}")):
 			return True
 		else:
 			return False
 
 	def createFile(self, folderName, fileName, fileType, contentToWrite):
+		'''Creates the file and writes content to it.
+		
+		Parameters:
+		----------
+		`folderName` : string
+			The name of the folder.
+		`fileName` : string
+			The name of the file.
+		`fileType` : string
+			The type/extension - json or txt.
+		`contentToWrite`:
+			The content to write to file.
+		
+		Returns:
+		-------
+		bool:
+			Returns True if file is created, False otherwise.
+		'''
 		if not self.returnFileExist(folderName, fileName, fileType): 
 			try:
 				newFile = open(self.dirPath + "/" + folderName +  "/" + fileName + "." + fileType, "w+")
@@ -24,6 +57,24 @@ class FileManager():
 			return False
 
 	def editFile(self, folderName, fileName, fileType, contentToWrite):
+		'''Edits the specified file, first checking if it exists.
+		
+		Parameters:
+		----------
+		`folderName` : string
+			The name of the folder.
+		`fileName` : string
+			The name of the file.
+		`fileType` : string
+			The type/extension - json or txt.
+		`contentToWrite`:
+			The content to write to file.
+		
+		Returns:
+		-------
+		bool:
+			Returns True if file is created, False otherwise.
+		'''
 		if self.returnFileExist(folderName, fileName, fileType): 
 			try:
 				existingFile = open(self.dirPath + "/" + folderName +  "/" + fileName + "." + fileType, "a+")
@@ -37,6 +88,22 @@ class FileManager():
 			return False
 
 	def readFile(self, folderName, fileName, fileType):
+		'''Reads the specified file.
+		
+		Parameters:
+		----------
+		`folderName` : string
+			The name of the folder.
+		`fileName` : string
+			The name of the file.
+		`fileType` : string
+			The type/extension - json or txt.
+		
+		Returns:
+		-------
+		bool(uknown ?):
+			Returns the content if file exists and can be read from, False otherwise.
+		'''
 		if self.returnFileExist(folderName, fileName, fileType): 
 			try:
 				existingFile = open(self.dirPath + "/" + folderName +  "/" + fileName + "." + fileType, "r")
@@ -50,6 +117,22 @@ class FileManager():
 			return False
 
 	def deleteFile(self, folderName, fileName, fileType):
+		'''Deletes the specified file.
+		
+		Parameters:
+		----------
+		`folderName` : string
+			The name of the folder.
+		`fileName` : string
+			The name of the file.
+		`fileType` : string
+			The type/extension - json or txt.
+		
+		Returns:
+		-------
+		bool:
+			Returns True if file exists and can be deleted, False otherwise.
+		'''
 		if self.returnFileExist(folderName, fileName, fileType):  
 			try:
 				os.remove(self.dirPath + "/" + folderName +  "/" + fileName + "." + fileType)
